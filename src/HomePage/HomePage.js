520f21d867b68375ef06ad312b89a24b8ed16b49
Tyler Ballesteros <tballest@uncc.edu>
2020-10-17 12:35:58 -0400

    Code updated for assignment 6

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,8 +1,50 @@
 import React from 'react';
+import Chart from "chart.js";
+import axios from 'axios';
 
 function HomePage() {
-  return (
-    <main className ="center" id="main">
+
+    var dataSource = {
+        datasets: [
+            {
+                data: [],
+                backgroundColor: [
+                    '#ffcd56',
+                    '#ff6384',
+                    '#36a2eb',
+                    '#fd6b19',
+                    '#03fce3',
+                    '#034efc',
+                    '#ab361f'
+                ],
+            }
+        ],
+        labels: []
+    };
+
+    function createChart(){
+        var ctx = document.getElementById("myChart").getContext("2d");
+        var myPieChart = new Chart(ctx, {
+            type: 'pie',
+            data: dataSource
+        });
+    }
+
+    function getBudget(){
+        axios.get('http://localhost:3000/budget')
+        .then(function (res) {
+            console.log(res);
+            for(var i = 0; i < res.data.myBudget.length; i++) {
+                dataSource.datasets[0].data[i] = res.data.myBudget[i].budget;
+                dataSource.labels[i] = res.data.myBudget[i].title;
+            }
+            createChart();
+        });
+    }
+        getBudget();
+
+  return(
+    <main className ="center">
 
         <div className="page-area">
 
@@ -35,31 +77,6 @@ function HomePage() {
                 <h1>Free</h1>
                 <p>
                     This app is <b>free!!!</b> And you are the only one holding your data!
-                </p>
-            </div>
-    
-            <div className="text-box">
-                <h1>Stay on track</h1>
-                <p>
-                    Do you know where you are spending your money? If you really stop to track it down,
-                    you would get surprised! Proper budget management depends on real data... and this
-                    app will help you with that!
-                </p>
-            </div>
-    
-            <div className="text-box">
-                <h1>Alerts</h1>
-                <p>
-                    What if your clothing budget ended? You will get an alert. The goal is to never go over the budget.
-                </p>
-            </div>
-    
-            <div className="text-box">
-                <h1>Results</h1>
-                <p>
-                    People who stick to a financial plan, budgeting every expense, get out of debt faster!
-                    Also, they to live happier lives... since they expend without guilt or fear... 
-                    because they know it is all good and accounted for.
                 </p>
             </div>
     
---
+++ src/server.js
@@ -0,0 +1,18 @@
+//Budget API
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get('/budget', (req, res) => {
+    const budget = require("./budget");
+    console.log(budget);
+    res.json(budget);
+});
+
+app.listen(port, () => {
+    console.log(`API served at http://localhost:${port}`)
+});
